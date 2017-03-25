0f37356be16fa7b0ea94d82ac616d3f9470dc428
Serge-Harnyk <xenonchikmaxxx@gmail.com>
2017-03-25 22:21:28 +0200

    Email fixes

--- src/main/java/app/proc/EmailProcessor.java
+++ src/main/java/app/proc/EmailProcessor.java
@@ -23,12 +23,12 @@ public class EmailProcessor {
     try {
       MimeMessageHelper helper = new MimeMessageHelper(message, 1, null);
 
-//      helper.setTo("ekhrenov@mail.ua");
+      helper.setTo("ekhrenov@mail.ua");
       helper.setTo("xenonchikmaxxx@gmail.com");
-//      helper.addCc("yuriy.ermak@yandex.ua");
+      helper.addCc("yuriy.ermak@yandex.ua");
 
-      helper.setText("Prices from " + new SimpleDateFormat("dd.MM.yyyy").format(new Date()));
-      helper.setSubject("Prices from " + new SimpleDateFormat("dd.MM.yyyy").format(new Date()));
+      helper.setText("Цены за " + new SimpleDateFormat("dd.MM.yyyy").format(new Date()));
+      helper.setSubject("Цены за " + new SimpleDateFormat("dd.MM.yyyy").format(new Date()));
       helper.setFrom("parser@blackbeholder.net");
       helper.addAttachment("prices.csv", new File("/opt/data/prices.csv"));
 
