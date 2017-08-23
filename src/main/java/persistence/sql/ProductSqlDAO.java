8891f42ec81478519026ec967f34e331024b0f8c
Serge-Harnyk <xenonchikmaxxx@gmail.com>
2017-08-23 15:30:26 +0300

    fix truncate

--- src/main/java/persistence/sql/ProductSqlDAO.java
+++ src/main/java/persistence/sql/ProductSqlDAO.java
@@ -59,7 +59,7 @@ public class ProductSqlDAO extends SqlDAO {
         beginTransaction();
         getSession().createSQLQuery("INSERT INTO archive.products_history (date, name, price, store, id, url, category, day, html)\n" +
                 "  SELECT date, name, price, store, id, url, category, day, html FROM products").executeUpdate();
-        getSession().createSQLQuery("truncate table MyTable").executeUpdate();
+        getSession().createSQLQuery("truncate table products").executeUpdate();
         endTransaction();
     }
 
